fd66e8c7c6e1bf6ea43419c7f456e807926d7a99
Dalton Le <daltonle.dev@gmail.com>
2024-12-27 12:28:28 +1100

    Update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended",
+  ),
+  {
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
